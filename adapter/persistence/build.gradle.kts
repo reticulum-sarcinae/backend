b8568d110c268174629ea29e9b7d31a85267f231
Leon Ehaus <leon.ehaus@engelvoelkers.com>
2026-08-25 15:26:38 +0200

    convert to gradle versions catalog

--- adapter/persistence/build.gradle.kts
+++ adapter/persistence/build.gradle.kts
@@ -1,11 +1,7 @@
-val springBootVersion: String by project
-val flywayVersion: String by project
-val maiadbClientVersion: String by project
-
 dependencies {
   implementation(project(":domain:port:persistence-port"))
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
-  implementation("org.springframework.boot:spring-boot-starter-flyway:$springBootVersion")
-  implementation("org.flywaydb:flyway-mysql:$flywayVersion")
-  implementation("org.mariadb.jdbc:mariadb-java-client:$maiadbClientVersion")
+  implementation(rootProject.libs.spring.boot.starter.data.jpa)
+  implementation(rootProject.libs.spring.boot.starter.flyway)
+  implementation(rootProject.libs.flyway.mysql)
+  implementation(rootProject.libs.mariadb.java.client)
 }
--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,19 +1,7 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
   java
-  id("org.springframework.boot")
-}
-
-val springBootVersion: String by project
-val springSecurityVersion: String by project
-val flywayVersion: String by project
-val mapstructVersion: String by project
-val testcontainersVersion: String by project
-val maiadbClientVersion: String by project
-
-tasks.named<BootJar>("bootJar") {
-  archiveFileName.set("app.jar")
+  libs.plugins.spring.boot
 }
 
 dependencies {
@@ -28,19 +16,19 @@ dependencies {
     runtimeOnly(project(adapterProject.path))
   }
 
-  implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-  implementation("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
-  implementation("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
-  implementation("org.mapstruct:mapstruct:$mapstructVersion")
+  implementation(rootProject.libs.spring.boot.starter.web)
+  implementation(rootProject.libs.spring.boot.starter.security)
+  implementation(rootProject.libs.spring.boot.starter.actuator)
+  implementation(rootProject.libs.mapstruct)
 
-  annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+  annotationProcessor(rootProject.libs.mapstruct.processor)
 
   testImplementation(project(":adapter:persistence"))
-  testImplementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
-  testImplementation("org.springframework.security:spring-security-test:$springSecurityVersion")
-  testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test:$springBootVersion")
+  testImplementation(rootProject.libs.spring.boot.starter.data.jpa)
+  testImplementation(rootProject.libs.spring.security.test)
+  testImplementation(rootProject.libs.spring.boot.starter.webmvc.test)
 
-  testImplementation("org.testcontainers:mariadb:$testcontainersVersion")
-  testImplementation("org.flywaydb:flyway-core:$flywayVersion")
-  testRuntimeOnly("org.mariadb.jdbc:mariadb-java-client:$maiadbClientVersion")
+  testImplementation(rootProject.libs.testcontainers.mariadb)
+  testImplementation(rootProject.libs.flyway.core)
+  testRuntimeOnly(rootProject.libs.mariadb.java.client)
 }
